03cc581ba2dc11ae5d76cae5a50d712bb142fde8
Pascal Stein <pascal.stein@gmail.com>
2021-03-08 13:52:35 +0000

    Updated dyndns cronjob to redirect wget output to /dev/null

--- dyndns-setup.sh
+++ dyndns-setup.sh
@@ -10,4 +10,4 @@ echo "Enter Domain Name"
 
 read DOMAIN_NAME
 
-(crontab -l 2>/dev/null; echo "*/30 * * * * wget 'https://zonomi.com/app/dns/dyndns.jsp?host=${DOMAIN_NAME}&api_key=${Z_API_KEY}'") | crontab -
+(crontab -l 2>/dev/null; echo "*/30 * * * * wget -O- 'https://zonomi.com/app/dns/dyndns.jsp?host=${DOMAIN_NAME}&api_key=${Z_API_KEY}' &> /dev/null") | crontab -
